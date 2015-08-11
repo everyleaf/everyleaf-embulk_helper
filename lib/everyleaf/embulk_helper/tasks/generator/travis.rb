06057a171d114859843d37f32e5b5be7fa2a17c3
uu59 <k@uu59.org>
2015-08-11 13:00:51 +0900

    Generate initial template for generate:travis

--- lib/everyleaf/embulk_helper/tasks/generator/travis.rb
+++ lib/everyleaf/embulk_helper/tasks/generator/travis.rb
@@ -19,11 +19,37 @@ module Everyleaf
           end
 
           def travis
+            init
             create_travis_yml
             logger.info "Updated .travis.yml"
           end
 
           private
+
+          def init
+            FileUtils.mkdir_p File.dirname(travis_yml_template_path)
+            unless File.exists?(travis_yml_template_path)
+              logger.info "Generate travis.yml template (#{travis_yml_template_path})"
+              File.open(travis_yml_template_path, "w") do |f|
+                f.write initial_template
+              end
+            end
+          end
+
+          def initial_template
+            <<-YML
+language: ruby
+rvm:
+  - jruby-19mode
+jdk:
+  - oraclejdk8
+
+gemfile:
+<% versions.each do |file| -%>
+  - gemfiles/<%= file %>
+<% end -%>
+            YML
+          end
 
           def travis_yml_template_path
             root_dir.join(options[:travis_yml_template] || DEFAULT_TRAVIS_YML_TEMPLATE)
